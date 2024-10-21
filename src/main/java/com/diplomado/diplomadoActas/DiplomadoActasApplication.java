6ec4287f9bcd768713637c410a0eadec588e135f
Andres Saenz <asaencordero@gmail.com>
2024-10-21 13:07:00 -0500

    inicializacion proyecto

---
+++ src/main/java/com/diplomado/diplomadoActas/DiplomadoActasApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.diplomadoActas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomadoActasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomadoActasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomado/diplomadoActas/DiplomadoActasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomado.diplomadoActas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomadoActasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
