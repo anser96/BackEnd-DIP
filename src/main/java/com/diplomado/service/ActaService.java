290004a2fe961b728411a42e6f35c2f0cd81dc4c
Andres Saenz <asaencordero@gmail.com>
2024-10-22 21:40:54 -0500

    Services para Acta

---
+++ src/main/java/com/diplomado/service/ActaService.java
@@ -0,0 +1,23 @@
+package com.diplomado.service;
+
+import com.diplomado.model.Acta;
+import com.diplomado.repository.ActaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActaService {
+
+    @Autowired
+    private ActaRepository actaRepository;
+
+    public Acta save(Acta acta) {
+        return actaRepository.save(acta);
+    }
+
+    public Acta aprobar(int actaId) {
+        Acta acta = actaRepository.findById(actaId).orElseThrow();
+        acta.setEstado("APROBADA");
+        return actaRepository.save(acta);
+    }
+}
